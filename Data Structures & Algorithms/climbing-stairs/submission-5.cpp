de3f0c0d5fc7832592ecb2c6cfa6d42a1e22758d
yxshag <sweetpepper281105@gmail.com>
2026-06-25 12:05:51 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        //first thing that we directly notice in this question 
+        //this is basically fibonacci series
+        //because for every value of n we basically have 2 options, we either take the last step as 2 or 1
+        //if we take last step as 2 then we have arr[n-2] ways in which we can climb , our last step being 2
+        //if we take last step as 1 then we have arr[n-1] ways in which we can climb , our last step being 1
+        //so this is a simple dp tabulation solution 
+
+        vector<int> memo(n+1);
+        memo[0]=1;
+        memo[1]=1;
+        for(int i=2;i<=n;i++){
+            memo[i]=memo[i-1]+memo[i-2];
+        }
+        return memo[n];
+    }
+};
