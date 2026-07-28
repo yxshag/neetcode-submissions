3a6807943c0e6c7358bdf51dd67641ac457cba77
yxshag <sweetpepper281105@gmail.com>
2026-07-28 15:56:19 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans=0;
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]-s[i+1]>=0){
+                ans+=s[i]-s[i+1];
+            }
+            else{
+                ans+=s[i+1]-s[i];
+            }
+        }
+        return ans;
+    }
+};
