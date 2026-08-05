939b25232c0ef9348ef2f7a8b397842610a8b770
yxshag <sweetpepper281105@gmail.com>
2026-08-05 22:51:22 +0530

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<int> s;
+        for(auto i:logs){
+            if(i=="../"){
+                if(!s.empty()){
+                    s.pop();
+                }
+            }
+            else if(i=="./"){
+                continue;
+            }
+            else{
+                s.push(1);
+            }
+        }
+        return s.size();
+    }
+};
