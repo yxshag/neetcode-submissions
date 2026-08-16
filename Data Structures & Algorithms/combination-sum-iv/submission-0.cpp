cbfe1a4584beff9b0996eaaecc9110753c889053
yxshag <sweetpepper281105@gmail.com>
2026-08-16 20:45:28 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, 0);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(auto j:nums){
+                if(j<=i){
+                    dp[i]+=dp[i-j];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
