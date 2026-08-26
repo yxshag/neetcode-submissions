c429f1872b41fbcfff432fc3c410c569fecccdb8
yxshag <sweetpepper281105@gmail.com>
2026-08-26 23:53:36 +0530

    Add: n-ary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/n-ary-tree-postorder-traversal/submission-0.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void traverse(Node* root, vector<int>& result) {
+        if (!root) return;
+        for (Node* child : root->children) {
+            traverse(child, result);
+        }
+        result.push_back(root->val);
+    }
+
+    vector<int> postorder(Node* root) {
+        vector<int> result;
+        traverse(root, result);
+        return result;
+    }
+};
